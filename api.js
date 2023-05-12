b7d96ee06648b7f864a1dffd804611ac6b87b501
James <jjguevarag@gmail.com>
2023-05-12 16:44:02 -0600

    update

--- api.js
+++ api.js
@@ -3,19 +3,35 @@ const user = require("./user.controller");
 const app = express();
 const port = 3000;
 
+// Se agrega la ruta para obtener todos los usuarios
 app.get("/", user.list);
+
+// Se agrega la ruta para crear un nuevo usuario
 app.post("/", user.create);
-app.get("/:id ", user.get);
-app.put("/:id ", user.upadte);
-app.patch("/:id ", user.upadte);
+
+// Se agrega la ruta para obtener un usuario por su ID
+app.get("/:id", user.get);
+
+// Se agrega la ruta para actualizar un usuario por su ID
+app.put("/:id", user.update);
+
+// Se corrige la ruta para actualizar un usuario por su ID (se cambia "upadte" a "update")
+app.patch("/:id", user.update);
+
+// Se agrega la ruta para eliminar un usuario por su ID
 app.delete("/:id", user.destroy);
+
+// Se agrega una ruta para manejar las solicitudes a rutas no existentes
 app.get("*", (req, res) => {
   res.status(404).send("Esta pagina no existe");
 });
+
+// Se agrega una ruta para manejar las solicitudes POST a rutas no existentes
 app.post("*", (req, res) => {
   res.status(404).send("Esta pagina no existe");
 });
 
+// Se inicia la escucha del servidor en el puerto indicado
 app.listen(port, () => {
   console.log("Arrancando la aplicacion");
 });
